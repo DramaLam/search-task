b61a0acd309be9548a1ff2442e30e518e97c9621
Milena K <123456milena@rambler.ru>
2024-03-07 19:29:54 +0300

    worked Search Yea

--- src/components/SearchFrom/SearchForm.tsx
+++ src/components/SearchFrom/SearchForm.tsx
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
-// import { FiterUsers } from "../FilterUsers/FilterUsers";
 import "./styles.css";
-import { SearchContext } from "../SearchResults/SearchContext";
-import { useContext } from "react";
-import useInput from "../../hooks/useInput";
-
 
 export function SearchForm({ onChange }) {
-  // const input = useInput()
-  // const [value, setValue] = useState('')
-  // const { users } = useContext(SearchContext);
-  // const [searchResults, setSearchResults] = React.useState([]);
   const handleChange = (event) => {
     onChange(event.target.value)
   }
-
-  // React.useEffect(() => {
-  //   const results = users.filter(user =>
-  //     user.firstName.toLowerCase().includes(value.toLowerCase())
-  //   );
-  //   setSearchResults(results);
-  // }, [value]);
 
   return (
     <div className="searchForm">
--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -1,44 +1,25 @@
 import { useContext } from "react";
 import { SearchContext } from "./SearchContext";
 import { UserCard } from "../UserCard/UserCard";
-import { SearchForm } from "../SearchFrom/SearchForm";
 import React, { useState } from "react";
-import useInput from "../../hooks/useInput";
 
 import "./style.css";
 
 export function SearchResults( { name } ) {
-  
   const { users } = useContext(SearchContext);
-    
-  const [value, setValue] = useState('')
-
-  // const input = useInput()
-
   const [searchResults, setSearchResults] = React.useState([]);
 
   console.log(name);
 
   React.useEffect(() => {
     const results = users.filter(user =>
-      user.firstName.toLowerCase().includes(value.toLowerCase())
+      user.firstName.toLowerCase().includes(name.toLowerCase())
     );
     setSearchResults(results);
-  }, [value]);
+  }, [name]);
 
   return (
     <div>
-      {/* <div className="searchForm">
-        <form>
-          <input 
-            type="text"
-            placeholder="Terry"
-            // value="Terry"
-            onChange={(event) => setValue(event.target.value)}
-            />
-        </form>
-      </div> */}
-    
       <div className="usersList">
         {searchResults.map((user) => (
           <UserCard {...user} />
